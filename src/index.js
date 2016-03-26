b954111eee0c8ebbd9330cdd3cb0bdc7df9a43a9
waltervargas <walter@flux7.com>
2016-03-26 01:17:41 -0430

    begining of sections 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyD1P9wIKGmyjtjcT2ghpdcuhgCCIkLp1dY';
 
+
+
 // Create the element
-const App = () => {
-    return (
-        <div>
-          <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = { videos: [] };
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos }); // { videos: videos }
+        });
+    }
+    render (){
+        return (
+            <div>
+            <SearchBar />
+            <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
 }
 
 // Load HTML Element into DOM
