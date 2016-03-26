5ca9f63f90ad76df1617aea545d741c0f45c5e64
waltervargas <walter@flux7.com>
2016-03-26 12:09:31 -0430

    Section 2: Video Selection

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyD1P9wIKGmyjtjcT2ghpdcuhgCCIkLp1dY';
 
@@ -13,16 +14,25 @@ const API_KEY = 'AIzaSyD1P9wIKGmyjtjcT2ghpdcuhgCCIkLp1dY';
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos }); // { videos: videos }
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
     render (){
         return (
             <div>
-            <SearchBar />
-            <VideoList videos={this.state.videos}/>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+                    videos={this.state.videos}/>
             </div>
         );
     }
